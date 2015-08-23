439a8a14233f5418db9272fffd600c7fcccb0191
Jannes Höke <jhoeke@uni-bremen.de>
2015-08-23 14:12:32 +0200

    fix directory creation

--- simple_share/share/views.py
+++ simple_share/share/views.py
@@ -11,23 +11,18 @@ UPLOAD_LOCATION = os.path.join('share', 'static', 'share', 'uploads')
 
 def index(request):
     # return render_to_response('index.html')
-    filelist = File.objects.order_by('-timestamp')
-    for f in filelist:
-        f.name = f.path.split('/')[-1]
-        f.path = '/'.join(['static', 'share', 'uploads', f.path])
-
     if request.method == 'POST':
         success = handle_uploaded_file(request.FILES['upload'], request.POST['note'], get_ip(request))
-        return render(request, 'index.html', {'upload': True, 'upload_success': success, 'filelist': filelist})
+        return render(request, 'index.html', {'upload': True, 'upload_success': success, 'filelist': get_filelist()})
     else:
-        return render(request, 'index.html', {'upload': False, 'filelist': filelist})
+        return render(request, 'index.html', {'upload': False, 'filelist': get_filelist()})
 
 def handle_uploaded_file(f, message, ip):
     try:
         dname = hex(int(time() * 10000000)).split('x')[1]
         fname = os.path.join(dname, str(f))
-        # os.mkdir(UPLOAD_LOCATION)
-        os.mkdir(os.path.join(UPLOAD_LOCATION, dname))
+        
+        os.makedirs(os.path.join(UPLOAD_LOCATION, dname))
 
         with open(os.path.join(UPLOAD_LOCATION, fname), 'wb+') as destination:
             for chunk in f.chunks():
@@ -41,3 +36,11 @@ def handle_uploaded_file(f, message, ip):
     except Exception as e:
         print(e)
         return False
+
+def get_filelist():
+    filelist = File.objects.order_by('-timestamp')
+    for f in filelist:
+        f.name = f.path.split('/')[-1]
+        f.path = '/'.join(['static', 'share', 'uploads', f.path])
+
+    return filelist
